39f519b989e69fe02d9c1afebf2ab3e37fd8e1bf
sachin <sachinsain21082004@gmail.com>
2023-01-19 20:44:27 +0530

    19-01-2023

---
+++ content remover.js
@@ -0,0 +1,29 @@
+let icon = document.querySelectorAll('i')
+for (let i = 0; i < icon.length; i++) {
+    icon[i].onclick = () => {
+        let child = icon[i].parentElement.parentElement.children
+        for (let j = 0; j < child.length; j++) {
+            if (j < child.length - 1) {
+                child[j].style.display = 'none'
+            }
+            else {
+                child[j].style.display = 'block'
+            }
+        }
+    }
+}
+let anchor = document.querySelectorAll('a')
+for (let m = 0; m < anchor.length; m++) {
+    anchor[m].onclick = (e) => {
+        e.preventDefault()
+        let child = anchor[m].parentElement.parentElement.children
+        for (let j = 0; j < child.length; j++) {
+            if (j < child.length - 1) {
+                child[j].style.display = 'block'
+            }
+            else {
+                child[j].style.display = 'none'
+            }
+        }
+    }
+}
