2839c47c7d8072f6d65622416016ad2c30128575
sachin <sachinsain21082004@gmail.com>
2023-01-20 00:43:17 +0530

    20-01-2023

--- content remover.js
+++ content remover.js
@@ -27,3 +27,32 @@ for (let m = 0; m < anchor.length; m++) {
         }
     }
 }
+let head = document.querySelectorAll('.head h1')
+for (let i = 0; i < head.length; i++) {
+    head[i].onclick = () => {
+        let child = head[i].parentElement.children
+        for (let j = 0; j < child.length; j++) {
+            if (j < child.length) {
+                child[j].style.display = 'none'
+            }
+            else {
+                child[j].style.display = 'block'
+            }
+        }
+    }
+}
+// let anchor = document.querySelectorAll('a')
+// for (let m = 0; m < anchor.length; m++) {
+//     anchor[m].onclick = (e) => {
+//         e.preventDefault()
+//         let child = anchor[m].parentElement.parentElement.children
+//         for (let j = 0; j < child.length; j++) {
+//             if (j < child.length - 1) {
+//                 child[j].style.display = 'block'
+//             }
+//             else {
+//                 child[j].style.display = 'none'
+//             }
+//         }
+//     }
+// }
